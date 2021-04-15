732604ebd008f1837e4dbea714602645d690a9b6
Jun Aruga <jaruga@redhat.com>
2021-04-15 17:08:23 +0200

    Update error_debug.rb.

--- spec/mysql2/error_debug.rb
+++ spec/mysql2/error_debug.rb
@@ -1,16 +1,12 @@
 # ruby spec/mysql2/error_debug.rb
 $LOAD_PATH << 'lib'
+
 require 'mysql2'
-require 'yaml'
-
-DatabaseCredentials = YAML.load_file('spec/configuration.yml')
-client = Mysql2::Client.new(DatabaseCredentials['root'])
-
-invalid_utf8 = ["e5c67d1f"].pack('H*').force_encoding(Encoding::UTF_8)
+client = Mysql2::Client.new(:host => "127.0.0.1", :port => '13306', :username => "root")
+str = ["e5c67d1f"].pack('H*')
+invalid_utf8 = str.force_encoding(Encoding::UTF_8)
 begin
   client.query(invalid_utf8)
 rescue Mysql2::Error => bad_err
-  # p bad_err.message.encoding == Encoding::UTF_8
-  p bad_err.message.include?("??}\u001F")
-  p "[DEBUG] bad_err.messag: #{bad_err.message}"
+  p bad_err.message
 end
